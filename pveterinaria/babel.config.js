0d0f7d824fa8f8a210cec4bda00b3297a718d498
keren31 <134246462+keren31@users.noreply.github.com>
2024-10-25 08:25:17 -0600

    prueba7 lint

--- pveterinaria/babel.config.js
+++ pveterinaria/babel.config.js
@@ -6,6 +6,7 @@ module.exports = {
     ],
     plugins: [
       '@babel/plugin-transform-runtime',
+      '@babel/plugin-proposal-private-methods',
       '@babel/plugin-proposal-private-property-in-object', // Agregar si es necesario
       '@babel/plugin-proposal-class-properties'
     ]
