e1c40b01be47978806e67b450852bc0430a8f444
OSwaAzu <20211318@uthh.edu.mx>
2024-10-25 09:50:33 -0600

    correccion de errores

--- pveterinaria/src/login1.js
+++ pveterinaria/src/login1.js
@@ -15,22 +15,18 @@ import imagen from './img/imagen1.jpg';
 import Layout from './Layout';
 
 export default function Login() {
-  //const apiurll ="http://localhost:5029/"
   const apiurll = "https://lacasadelmariscoweb.azurewebsites.net/";
   const { loginUser } = useUser();
   const [passwordVisible, setPasswordVisible] = useState(false);
   const [email, setEmail] = useState("");
-  const [ip] = useState("788.990.899.00");
-
   const [password, setPassword] = useState("");
   const [emailError, setEmailError] = useState("");
   const [passwordError, setPasswordError] = useState("");
   const [loginAttempts, setLoginAttempts] = useState(0);
-  const ClientID = "30463532374-6m31aqpp06eqco9k3325unc6n62cs8ej.apps.googleusercontent.com"
+  const ClientID = "30463532374-6m31aqpp06eqco9k3325unc6n62cs8ej.apps.googleusercontent.com";
   const [isButtonDisabled, setIsButtonDisabled] = useState(true);
 
   const navigate = useNavigate();
-  
 
   const togglePasswordVisibility = () => {
     setPasswordVisible(!passwordVisible);
@@ -43,9 +39,6 @@ export default function Login() {
   const handleBlur = () => {
     validatePassword(password);
   };
-
-
-  //
 
   const validateEmail = (email) => {
     const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
@@ -71,12 +64,12 @@ export default function Login() {
       return false;
     }
   };
+
   const handleSubmit = async (event) => {
     event.preventDefault();
     const data = new FormData();  
     data.append('Correo', email);
     data.append('Contrasena', password);
-    data.append('ip', ip);
     
     if (validateEmail(email) && validatePassword(password)) {
       try {
@@ -93,31 +86,21 @@ export default function Login() {
             setIsButtonDisabled(true);
             await fetch(apiurll+"api/CasaDelMarisco/BloquearCuenta?Correo=" + email, {
               method: "POST",
-              body: data,
             });
           }
-        }else if (result === 'Error en las credenciales') {
-          setEmailError('Error en las credenciales');
-        }else if (result === 'Contraseña correcta') {
+        } else if (result === 'Contraseña correcta') {
           const userData = await obtenerDatosUsuario(email);
           loginUser(userData);
-          fetch( 'http://localhost:5029/api/CasaDelMarisco/ActualizarTokenEstetica', {
-            method: "POST",
-            body: data,
-          });
-          localStorage.setItem("userData", JSON.stringify({ email }));
-          navigate('/multifactor')
-         
+          navigate('/multifactor');
         } else if (result === 'Contraseña correcta para administrador') {
-          const userData = await obtenerDatosUsuario();
+          const userData = await obtenerDatosUsuario(email);
           loginUser(userData);
           Swal.fire({
             icon: 'success',
-            title: 'Bienvenido de nuevo Administrador!: '+userData.Nombre,
-            text: 'Ahora ve y navega en nuestra paguina.',
+            title: `Bienvenido de nuevo Administrador: ${userData.Nombre}`,
+            text: 'Ahora ve y navega en nuestra página.',
           });
-          navigate('/admin-citas')
-          
+          navigate('/admin-citas');
         } else {
           setPasswordError('Su cuenta está bloqueada');
         }
@@ -128,11 +111,9 @@ export default function Login() {
       console.log('Formulario no válido');
     }
   };
-  
 
-  function onChange(value) {
+  function onChange() {
     setIsButtonDisabled(false);
-
   }
   
   useEffect(() => {
@@ -142,61 +123,31 @@ export default function Login() {
       });
     };
     gapi.load("client:auth2", start);
-  }, []); // Agrega 'ObtenerIp' en el array de dependencias
+  }, []);
   
-
   const onSuccess = async (response) => {
     const email = response.profileObj.email;
     const data = new FormData();
     data.append("Correo", email);
-    data.append("ip", ip);
 
-    fetch(apiurll + "api/CasaDelMarisco/VerificarCorreo", {
-      method: "POST",
-      body: data,
-  })
-  .then((res) => res.json())
-  .then(async (result) => {
+    try {
+      const result = await fetch(apiurll + "api/CasaDelMarisco/VerificarCorreo", {
+        method: "POST",
+        body: data,
+      }).then((res) => res.json());
+      
       if (result === "Correo Existe") {
-          const resultado = await obtenerDatosUsuario(email);
-
-          // Segunda llamada fetch
-          fetch(apiurll + "api/CasaDelMarisco/LoginOauth", {
-              method: "POST",
-              body: data,
-          })
-          .then((res) => res.json())
-          .then((loginResult) => {
-              loginUser(resultado);
-              if (resultado.Rol === 2) {
-                  Swal.fire({
-                      icon: "success",
-                      title: "Login de administrador",
-                      text: "Cuidado, eres administrador. Puedes modificar datos de la página, siempre con cuidado.",
-                  });
-                  navigate("/Pedidos_admin");
-              } else {
-                  navigate("/");
-              }
-              //console.log(loginResult);
-          })
-          .catch((error) => {
-              console.error("Error en la segunda llamada fetch:", error);
-          });
-      } else {
-          // Aquí puedes manejar el caso en el que el correo no existe
+        const userData = await obtenerDatosUsuario(email);
+        loginUser(userData);
+        navigate(userData.Rol === 2 ? "/Pedidos_admin" : "/");
       }
-  })
-  .catch((error) => {
-      console.error("Error en la primera llamada fetch:", error);
-  });
-
-  //console.log(response);
-};
-
+    } catch (error) {
+      console.error("Error en la verificación de correo:", error);
+    }
+  };
 
   const onFailure = () => {
-    console.log("Algo salio mal");
+    console.log("Algo salió mal");
   };
 
   const obtenerDatosUsuario = async (email) => {
@@ -210,10 +161,7 @@ export default function Login() {
       if (response.ok) {
         return await response.json();
       } else {
-        console.error(
-          "Error al obtener datos del usuario que ingresaste:",
-          response.statusText
-        );
+        console.error("Error al obtener datos del usuario:", response.statusText);
         return null;
       }
     } catch (error) {
@@ -222,16 +170,11 @@ export default function Login() {
     }
   };
 
-  
   return (
     <Layout>
       <div className="registro-form-containerLogin">
         <div className="registro-image-containerLogin w-full lg:w-1/2 flex justify-center lg:justify-end">
-          <img
-            src={imagen}
-            alt="Registro"
-            className="registro-imageLogin w-full max-w-xs lg:max-w-md h-auto"
-          />
+          <img src={imagen} alt="Registro" className="registro-imageLogin w-full max-w-xs lg:max-w-md h-auto" />
         </div>
 
         <div className="registro-formLogin w-full lg:w-1/2">
@@ -244,9 +187,7 @@ export default function Login() {
           </label>
 
           <form onSubmit={handleSubmit}>
-            <label htmlFor="email" className="loginLabel">
-              Correo electrónico :
-            </label>
+            <label htmlFor="email" className="loginLabel">Correo electrónico :</label>
             <input
               type="email"
               id="email"
@@ -259,41 +200,28 @@ export default function Login() {
             />
             {emailError && <p className="error-message">{emailError}</p>}
 
-            <label htmlFor="email" className="loginLabel">
-              Contraseña :
-            </label>
-
+            <label htmlFor="password" className="loginLabel">Contraseña :</label>
             <div className="password-input-container">
-  <input
-    type={passwordVisible ? "text" : "password"}
-    id="password"
-    name="password"
-    value={password}
-    required
-    onChange={handlePasswordChange}
-    onBlur={handleBlur}
-    className={passwordError ? "input-error" : ""}
-  />
-  <button
-    type="button"
-    onClick={togglePasswordVisibility}
-    className="btn-visibility"
-  >
-    {passwordVisible ? (
-      <VisibilityOutlinedIcon fontSize="small" />
-    ) : (
-      <VisibilityOffOutlinedIcon fontSize="small" />
-    )}
-  </button>
-</div>
+              <input
+                type={passwordVisible ? "text" : "password"}
+                id="password"
+                name="password"
+                value={password}
+                onChange={handlePasswordChange}
+                onBlur={handleBlur}
+                className={passwordError ? "input-error" : ""}
+                required
+              />
+              <button type="button" onClick={togglePasswordVisibility} className="btn-visibility">
+                {passwordVisible ? <VisibilityOutlinedIcon fontSize="small" /> : <VisibilityOffOutlinedIcon fontSize="small" />}
+              </button>
+            </div>
             {passwordError && <p className="error-message">{passwordError}</p>}
 
-
             <div className="recuerdame">
-  <input type="checkbox" id="rememberMe" className="cuadro" />
-  <label htmlFor="rememberMe">Recuérdame</label>
-</div>
-
+              <input type="checkbox" id="rememberMe" className="cuadro" />
+              <label htmlFor="rememberMe">Recuérdame</label>
+            </div>
 
             <GoogleLogin
               clientId={ClientID}
@@ -311,25 +239,17 @@ export default function Login() {
               />
             </div>
 
-            <button
-              className="btn btn-warning"
-              type="submit"
-              disabled={isButtonDisabled}
-            >
+            <button className="btn btn-warning" type="submit" disabled={isButtonDisabled}>
               Entrar
             </button>
           </form>
 
           <div className="container">
-            <Link to="/recuperar" className="singText">
-              ¿Olvidaste tu password?
-            </Link>
-            <Link to="/registro" className="singText ms-3">
-              ¿Sin cuenta? Registrate
-            </Link>
+            <Link to="/recuperar" className="singText">¿Olvidaste tu password?</Link>
+            <Link to="/registro" className="singText ms-3">¿Sin cuenta? Regístrate</Link>
           </div>
         </div>
       </div>
     </Layout>
   );
-};
+}
--- pveterinaria/src/pagina_error.js
+++ pveterinaria/src/pagina_error.js
@@ -2,33 +2,28 @@ import React from 'react';
 import './css/PaguinaError.css'; // Importa tu archivo de estilos CSS aquí
 
 const Error404 = () => {
-    return (
-      <div className='todo'>
+  return (
+    <div className='todo'>
+      <img src="https://assets.codepen.io/1538474/404.svg" className="logo-404" alt="404" />
+      <img src="https://assets.codepen.io/1538474/meteor.svg" className="meteor" alt="Meteor" />
+      <div className=""></div>
+      
+      <p className="title">Oh no!!</p>
+      <p className="subtitle">
+        Has ingresado un URL inválido <br /> o una página que ya no existe.
+      </p>
 
-<img src="https://assets.codepen.io/1538474/404.svg" className="logo-404" alt="404" />
-        <img src="https://assets.codepen.io/1538474/meteor.svg" className="meteor" alt="Meteor" />
-        <div className=""></div>
-        
-       
-        <p className="title">Oh no!!</p>
-        <p className="subtitle">
-          Has ingresado un URL invalido <br /> o una paguina que ya no existe .
-
-
-          <div align="center">
-          <br/><br/>
-          <a href="./productos" className="btn-back">Verifique si la ruta de la paguina que consulto es correcta 
-          o de click para regresar a la paguina anterior</a>
-          <br/><br/><br/><br/>
-          <img src="https://assets.codepen.io/1538474/astronaut.svg" className="astronaut" alt="Astronaut" />
-        
-        </div>
-        </p>
-        
-        
-        
+      {/* Centrado utilizando estilos en línea */}
+      <div style={{ textAlign: 'center' }}>
+        <br /><br />
+        <a href="./productos" className="btn-back">
+          Verifique si la ruta de la página que consultó es correcta o haga clic para regresar a la página anterior
+        </a>
+        <br /><br /><br /><br />
+        <img src="https://assets.codepen.io/1538474/astronaut.svg" className="astronaut" alt="Astronaut" />
       </div>
-    );
-  }
-  
-  export default Error404;
+    </div>
+  );
+}
+
+export default Error404;
